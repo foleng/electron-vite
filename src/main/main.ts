a61d9b5e801b1cb8bc6efb817ec61b8b90b1dc67
felong <huangyi@tungee.com>
2022-08-04 00:26:47 +0800

    feat:init

---
+++ src/main/main.ts
@@ -0,0 +1,34 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+process.env
+console.log(process);
+
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.ts')
+    }
+  })
+
+  win.loadURL('http://127.0.0.1:5174/')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
